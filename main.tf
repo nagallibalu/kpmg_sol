912fab7e97fcb1a9e8b436ca6baea69607556a8c
nagallibalu <40028667+nagallibalu@users.noreply.github.com>
2022-07-04 18:51:07 +0530

    chllenge_1  multitier application apigateway lam

---
+++ main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+   region = "ap-south-1"
+}
+
+resource "aws_lambda_function" "copylambda" {
+   function_name = "copys3filelambda"
+
+   
+   s3_bucket = "bala-lambdasource"
+   s3_key    = "copys3file.zip"
+
+   
+   handler = "lambda.handler"
+   runtime = "python3.8"
+
+   role = "arn:aws:iam::210663491402:role/service-role/s3fullaccess"    ##using existing role
+}
+
+resource "aws_api_gateway_rest_api" "apiLambda" {
+  name        = "s3filecopyAPI"
+}
+
+
+resource "aws_api_gateway_resource" "proxy" {
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   parent_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+   path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxyMethod" {
+   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+   resource_id   = aws_api_gateway_resource.proxy.id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   resource_id = aws_api_gateway_method.proxyMethod.resource_id
+   http_method = aws_api_gateway_method.proxyMethod.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.copylambda.invoke_arn
+}
+
+
+
+
+resource "aws_api_gateway_method" "proxy_root" {
+   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+   resource_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   resource_id = aws_api_gateway_method.proxy_root.resource_id
+   http_method = aws_api_gateway_method.proxy_root.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.copylambda.invoke_arn
+}
+
+
+resource "aws_api_gateway_deployment" "apideploy" {
+   depends_on = [
+     aws_api_gateway_integration.lambda,
+     aws_api_gateway_integration.lambda_root,
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   stage_name  = "test"
+}
+
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.copylambda.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/*"
+}
+
+
+output "base_url" {
+  value = aws_api_gateway_deployment.apideploy.invoke_url
+}
